a27b3c2f340ee5d0d230e3ed841fcf2bdaf4be80
Saadallah <saadallah.kassir@gmail.com>
2019-11-27 14:23:21 -0600

    test commit new branch

--- reinforce.py
+++ reinforce.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
 
     B = Baseline(0.)
 
-    G = REINFORCE(env, gamma, 10000, pi,B)
+    G = REINFORCE(env, gamma, 50000, pi,B)
     obs = env.reset()
     print(obs)
     print("Position of max SINR in SNR array")
